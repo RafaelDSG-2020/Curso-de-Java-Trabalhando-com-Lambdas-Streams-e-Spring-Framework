d63b6eba7ce31be97a2f65c71422dbef42a45b16
Rafael da Silva <rafagomes53@alu.ufc.br>
2024-01-14 21:01:22 -0300

    Colocando o Banco no Properties

--- src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("Sua chave aqui");
+        OpenAiService service = new OpenAiService("chave aqui");
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
