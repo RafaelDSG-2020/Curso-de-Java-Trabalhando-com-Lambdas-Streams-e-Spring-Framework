5d05504080d8428e9adb299cd037d088d5323e21
Rafael da Silva <rafagomes53@alu.ufc.br>
2024-01-25 14:50:58 -0300

    Implementando Segurança em dados Sensiveis

--- src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("");
+        OpenAiService service = new OpenAiService(System.getenv("OPENAI_APIKEY"));
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
